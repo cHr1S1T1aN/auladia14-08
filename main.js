e4af4ad1c6c704abe6fb6f010c8a82cb4317385b
cHr1S1T1aN <chrismorats50918@gmail.com>
2025-08-14 19:53:36 -0300

    test:testando javascript da aula da faculdade

---
+++ main.js
@@ -0,0 +1,22 @@
+/* declarando variaveis*/
+var a ="afonso";
+let b ="aguua";
+const texto ="ola";
+
+
+
+/*Funções*/
+
+/*function soma(a,b){
+    return a+b;
+}
+console.log(soma(1,2));*/
+
+
+/*lista*/
+
+var teste= [1,2,3]
+console.log(teste)//toda lista
+console.log(teste[0])
+console.log(teste[1])
+console.log(teste[2])
