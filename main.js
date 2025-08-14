cbf01f511f801f1d1bcd4ca6ad290e35d8885767
cHr1S1T1aN <chrismorats50918@gmail.com>
2025-08-14 20:35:01 -0300

    arrumando besteira

--- main.js
+++ main.js
@@ -16,9 +16,32 @@ console.log(soma(1,2));*/
 /*lista*/
 
 var teste= [1,2,3]
+var testedois =[1,2]
 console.log(teste)//toda lista
 console.log(teste[0])
 console.log(teste[1])
 console.log(teste[2])
+teste.push(1)//coloca no final da lista
 
-console.log(teste+a)
+
+/*Objeto*/
+var aluno ={
+    nome:["aluno","chris","japones"],
+    aprovado:[false,true]
+}
+var alunoDois ={
+    nome:"CARLINHOS",
+    aprovado:true
+}
+var alunotres ={
+    nome:"Minecraft",
+    aprovado:false
+}
+
+
+/* 
+console.log(aluno,alunoDois)
+console.log(aluno.nome)*/
+
+var listaum= [aluno,alunoDois,alunotres]
+console.log(listaum)
