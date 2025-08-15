92908f83b9cfab6b867ecdb2347187465902f380
cHr1S1T1aN <chrismorats50918@gmail.com>
2025-08-14 21:46:54 -0300

    teste

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+//WebView
+
+
+
+
+
+
+
 /* declarando variaveis*/
-var a ="afonso";
+/*var a ="afonso";
 let b ="aguua";
 const texto ="ola";
 
-
+*/
 
 /*Funções*/
 
@@ -13,7 +21,7 @@ const texto ="ola";
 console.log(soma(1,2));*/
 
 
-/*lista*/
+/*lista
 
 var teste= [1,2,3]
 var testedois =[1,2]
@@ -23,8 +31,9 @@ console.log(teste[1])
 console.log(teste[2])
 teste.push(1)//coloca no final da lista
 
-
+*/
 /*Objeto*/
+/*
 var aluno ={
 
     nome:["aluno","chris","japones","cavalo"],
@@ -38,11 +47,88 @@ var alunotres ={
     nome:"Minecraft",
     aprovado:false
 }
-
-
+*/
 /* 
 console.log(aluno,alunoDois)
-console.log(aluno.nome)*/
+console.log(aluno.nome)
 
 var listaum= [aluno,alunoDois,alunotres]
 console.log(listaum)
+*/
+
+/*array com Objeto
+var teste =[
+    {
+        nome:"aluno",
+        aprovado:[false,true]
+    },
+    {
+    nome:"chris",
+    aprovadoo:true
+    },
+    {
+        nome:"Minecraft",
+        aprovado:[false,true]
+    }
+]
+console.log(teste)
+console.log(teste[0])
+console.log(teste[0].nome)
+
+*/
+
+
+
+
+/*
+//ARROW FUNCTION
+const somaDois=(a,b)=>{
+    return a+b
+}
+console.log(somaDois(7,6))
+
+*/
+var a =1
+var b=2
+var c=3
+var d=4
+/*
+const divisao=(a,b)=>{
+    return a/b
+}
+const multiplicacao=(b,c)=>{
+    return b.c
+}
+function soma(d,e){
+    return d+e
+}
+function subtracacao(a,b){
+    return a-b
+}
+
+console.log(divisao(a,b))
+console.log(multiplicacao(a,d))
+console.log(soma(d,b))
+console.log(subtracacao(c,a))
+*/
+var carros =[
+{
+    modelo: "nissan",
+    ano:2010,
+    cor:"preto"
+},
+{
+    modelo: "volks",
+    ano:2012,
+    cor:"azul"
+},
+{
+    modelo: "sedan",
+    ano:2020,
+    cor:"verde"
+},
+]
+
+console.log(carros)
+console.log(carros[0])
+console.log(carros[1].modelo)
